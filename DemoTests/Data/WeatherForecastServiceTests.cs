6bc00d4bc800c2c8b859d4a96f106aac61db1b15
Brent Ricks <brent.ricks@igmal.com>
2024-09-02 22:43:08 -0600

    Add project files.

---
+++ DemoTests/Data/WeatherForecastServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Demo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Data.Tests
+{
+    [TestClass()]
+    public class WeatherForecastServiceTests
+    {
+        [TestMethod()]
+        public void GetForecastAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
